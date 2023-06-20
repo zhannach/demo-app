7b5209f52970513103725aa190796249f45f3f31
zhannach <zhanna.chaykovska@gmail.com>
2023-06-20 18:20:09 +0300

    feat: add Layout and Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Wrapper } from './Header.style'
+
+const Header = () => {
+  return (
+    <Wrapper>
+      <h3>InCode</h3>
+      <h5>Finance</h5>
+    </Wrapper>
+  )
+}
+
+export default Header
